ea1a223f52e0493bc10602018abcd3e36efa5a7f
sineverba <alessandro.arici@gmail.com>
2020-04-16 18:57:52 +0200

    Part 1.4

--- project/__init__.py
+++ project/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
 
@@ -7,7 +8,9 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+#app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 class Ping(Resource):
     def get(self):
