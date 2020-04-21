87ab3a73d283661bf0ca42e73936d383c4500599
sineverba <alessandro.arici@gmail.com>
2020-04-21 21:45:49 +0200

    Release 2

--- project/api/ping.py
+++ project/api/ping.py
@@ -7,7 +7,7 @@ api = Api(ping_blueprint)
 
 class Ping(Resource):
     def get(self):
-        return {"status": "success", "message": "pong! New release done!"}
+        return {"status": "success", "message": "pong! 2020-04-21 19:45:00 + 02.00"}
 
 
 api.add_resource(Ping, "/ping")
