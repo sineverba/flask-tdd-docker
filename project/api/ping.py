a8a539b7c964625e1ba02939fdda2a0d6a1901d7
sineverba <alessandro.arici@gmail.com>
2020-04-22 12:52:28 +0200

    Force push

--- project/api/ping.py
+++ project/api/ping.py
@@ -7,7 +7,7 @@ api = Api(ping_blueprint)
 
 class Ping(Resource):
     def get(self):
-        return {"status": "success", "message": "pong! 22041225"}
+        return {"status": "success", "message": "pong! 22041252"}
 
 
 api.add_resource(Ping, "/ping")
