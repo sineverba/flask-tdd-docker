5a6509460b6c4c147702acd7c1d471f4fc8e38f3
sineverba <alessandro.arici@gmail.com>
2020-04-23 11:52:33 +0200

    Update Travis

---
+++ .travis_test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker exec app python -m pytest "project/tests";
+docker exec app flake8 project;
+docker exec app black project --check;
+docker exec app isort project/**/*.py --check-only;
