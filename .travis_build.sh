98136777b422c9266e08a82ae29687ec4db2486b
sineverba <alessandro.arici@gmail.com>
2020-04-23 11:41:25 +0200

    Update Travis

--- .travis_build.sh
+++ .travis_build.sh
@@ -6,9 +6,17 @@ docker network create -d bridge test-net
 
 docker run -d \
   --name db \
-  -e POSTGRES_USER=$DATABASE_TEST_USER \
-  -e POSTGRES_PASSWORD=$DATABASE_TEST_PASSWORD \
-  -e POSTGRES_DB=$DATABASE_TEST_NAME \
-  -p 5432:5432 \
+  -e POSTGRES_USER=user \
+  -e POSTGRES_PASSWORD=user \
+  -e POSTGRES_DB=users \
+  -p 54321:5432 \
   --network=test-net \
   postgres:12-alpine
+
+docker run -d \
+  --name app \
+  -e "PORT=8765" \
+  -e "DATABASE_TEST_URL=postgresql://user:user@db:54321/users" \
+  -p 5002:8765 \
+  --network=test-net \
+  $HEROKU_REGISTRY_IMAGE;
