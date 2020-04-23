4f1fe6c53c4591acda275393b1ddf78f9a588bab
sineverba <alessandro.arici@gmail.com>
2020-04-23 12:25:52 +0200

    Update Travis

--- .travis_build.sh
+++ .travis_build.sh
@@ -16,8 +16,7 @@ docker run -d \
 docker run -d \
   --name app \
   -e "PORT=8765" \
-  -e "DATABASE_TEST_URL=postgresql://postgres:postgres@db:54321/users" \
-  -e "DATABASE_URL=postgresql://postgres:postgres@db:54321/users" \
+  -e "DATABASE_TEST_URL=postgresql://postgres:postgres@db:5432/users" \
   -p 5002:8765 \
   --network=test-net \
   $HEROKU_REGISTRY_IMAGE;
