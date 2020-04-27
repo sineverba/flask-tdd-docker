0d414d788d19530409a4eeb83be4ef32d3e23deb
sineverba <alessandro.arici@gmail.com>
2020-04-27 20:30:28 +0200

    Clean a bit

--- .travis_scripts/travis_deploy.sh
+++ .travis_scripts/travis_deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-docker login -u _ -p $HEROKU_AUTH_TOKEN registry.heroku.com;
+# docker login -u _ -p $HEROKU_AUTH_TOKEN registry.heroku.com;
+docker login -u _ -p $HEROKU_API_KEY registry.heroku.com;
 docker push $HEROKU_REGISTRY_IMAGE;
 IMAGE_ID=$(docker inspect ${HEROKU_REGISTRY_IMAGE} --format={{.Id}})
 PAYLOAD='{"updates": [{"type": "web", "docker_image": "'"$IMAGE_ID"'"}]}'
@@ -9,7 +10,7 @@ curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP_NAME/formation \
   -d "${PAYLOAD}" \
   -H "Content-Type: application/json" \
   -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
-  -H "Authorization: Bearer ${HEROKU_AUTH_TOKEN}";
+  -H "Authorization: Bearer ${HEROKU_API_KEY}";
 
 curl https://cli-assets.heroku.com/install-ubuntu.sh | sh;
 heroku run python manage.py db migrate --app $HEROKU_APP_NAME;
