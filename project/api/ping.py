04e41dff75244e96d439605c0eee93b37bc233d4
sineverba <alessandro.arici@gmail.com>
2020-04-27 19:38:47 +0200

    Test travis

--- project/api/ping.py
+++ project/api/ping.py
@@ -7,7 +7,7 @@ class Ping(Resource):
     def get(self):
         return {
             "status": "success",
-            "message": "pong! V 0.6.0 - 2020-04-27 12.49 - Add Migrations",
+            "message": "pong! V 0.7.2 - 2020-04-27 19.33 - Update Travis",
         }
 
 
